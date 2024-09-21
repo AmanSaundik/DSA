f07d8356d94f2eadac132cf92f53131b1671317c
AmanSaundik <amansaundik802214@gmail.com>
2024-09-21 13:09:20 +0530

    Create InvertedNumberHalfPyramid.java

---
+++ Patterns/InvertedNumberHalfPyramid.java
@@ -0,0 +1,14 @@
+public class InvertedNumberHalfPyramid{
+    public static void main(String[] args) {
+        int n = 5;
+        
+        for(int i = 1; i <= n; i++){
+            int k = 1;
+            for(int j = 5; j>=i; j--){
+                    System.out.print(k);
+                    k++;
+            }
+            System.out.println("");
+        }
+    }
+}
