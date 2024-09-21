465f1aa8a1562aa3b5dc33eea971a762e5157002
AmanSaundik <amansaundik802214@gmail.com>
2024-09-21 12:57:52 +0530

    Create rectangle.java

---
+++ Patterns/rectangle.java
@@ -0,0 +1,12 @@
+public class rectangle {
+    public static void main(String args[]) {
+        int m = 4;
+        int n = 5;
+        for (int i = 0; i < m; i++) {
+            for (int k = 0; k < n; k++) {
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+    }
+}
