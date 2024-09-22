e319f87f1252f8694ede211c0a4dd0ed0cbaa7a5
AmanSaundik <amansaundik802214@gmail.com>
2024-09-22 18:14:54 +0530

    Create hollowpyramid.java

---
+++ Patterns/hollowpyramid.java
@@ -0,0 +1,25 @@
+public class hollowpyramid{
+    public static void main(String[] args) {
+        int n = 5;
+        for(int i = 1; i <= n; i++){
+            for(int j = n; j>=i; j--){
+                System.out.print(" ");
+            
+            }
+            for(int k = 1; k <= i; k++){
+                if(i>1 && i<n){
+                if(k>1 && k<i){
+                    System.out.print("  ");
+                }
+                else{
+                    System.out.print("* ");
+                }
+                }
+                else{
+                    System.out.print("* ");
+                }
+            }
+            System.out.println("");
+        }
+    }
+}
